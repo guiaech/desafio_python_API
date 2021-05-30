cbe462d70e4f948a6a58e1fcd85505ced85f6794
Guilherme Andrade e Chaves <83043492+guiaech@users.noreply.github.com>
2021-05-30 02:15:18 -0300

    Add files via upload

---
+++ desafio_mr.py
@@ -0,0 +1,24 @@
+import os
+import time
+import json
+import requests
+
+x = 0
+while x <= 1:
+    cotacoes_bit = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR")
+    cotacoes_bit = cotacoes_bit.json()
+    cotacao_dolar = cotacoes_bit["USD"]
+    print(f"Valor da cotação do BTCUSD é :${cotacao_dolar}")
+
+    cotacoes_eth = requests.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,JPY,EUR")
+    cotacoes_eth = cotacoes_eth.json()
+    cotacaoeth_dolar = cotacoes_eth["USD"]
+    print(f"Valor da cotação do ETHUSD é :${cotacaoeth_dolar}")
+
+    cotacoes_ltc = requests.get("https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD,JPY,EUR")
+    cotacoes_ltc = cotacoes_ltc.json()
+    cotacaoltc_dolar = cotacoes_ltc["USD"]
+    print(f"Valor da cotação do LTCUSD é :${cotacaoltc_dolar}")
+
+    time.sleep(30)
+    os.system('clear')
